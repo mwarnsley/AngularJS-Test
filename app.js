6193a277d0f908266e836fa31238be1779e9413a
mwarnsley <marcus.warnsley@gmail.com>
2016-03-07 13:41:40 -0500

    Adding the node express server for testing along with adding a templates folder to serve the different views

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
-var info = [
-    {
-        domain: 'facebook.com',
-        status: "Grow your current relationship",
-        image_url: "http://i.telegraph.co.uk/multimedia/archive/03474/Facebook_3474124b.jpg"
-    },
-    {
-        domain: 'instagram.com',
-        status: "Create a new relationship",
-        image_url: "http://marketingland.com/wp-content/ml-loads/2014/07/instagram-iphone-app-1920.jpg"
-    },
-    {
-        domain: 'snapchat.com',
-        status: "Grow your current relationship",
-        image_url: "http://media3.popsugar-assets.com/files/2015/11/10/090/n/1922398/74f83cd48e2f90c8_11311496_779715442138490_1431060595_n.xxxlarge/i/Celebrities-Follow-Snapchat.jpg"
-    },
-    {
-        domain: 'twitter.com',
-        status: "Create a new relationship",
-        image_url: "http://i.telegraph.co.uk/multimedia/archive/03375/twitter2_3375790k.jpg"
-    }
-];
+var express = require("express");
 
-var myApp = angular.module('myApp', []);
+var app = express();
 
-myApp.directive('myFirstDirective', function(){
-    return{
-        template: "<p>this is some content</p>",
-        restrict: "E"
+var port = 8000;
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, function(err){
+    if(err){
+        console.log("There was an error starting the Server on port: " + port);
+    }else{
+        console.log("Server is running on port: " + port);
     }
 });
---
+++ js/main.js
@@ -0,0 +1,38 @@
+var info = [
+    {
+        domain: 'facebook.com',
+        status: "Grow your current relationship",
+        image_url: "http://i.telegraph.co.uk/multimedia/archive/03474/Facebook_3474124b.jpg"
+    },
+    {
+        domain: 'instagram.com',
+        status: "Create a new relationship",
+        image_url: "http://marketingland.com/wp-content/ml-loads/2014/07/instagram-iphone-app-1920.jpg"
+    },
+    {
+        domain: 'snapchat.com',
+        status: "Grow your current relationship",
+        image_url: "http://media3.popsugar-assets.com/files/2015/11/10/090/n/1922398/74f83cd48e2f90c8_11311496_779715442138490_1431060595_n.xxxlarge/i/Celebrities-Follow-Snapchat.jpg"
+    },
+    {
+        domain: 'twitter.com',
+        status: "Create a new relationship",
+        image_url: "http://i.telegraph.co.uk/multimedia/archive/03375/twitter2_3375790k.jpg"
+    }
+];
+
+var myApp = angular.module('myApp', ['ui.router'])
+
+    .config(function($stateProvider, $urlRouterProvider){
+        $stateProvider.state('opportunities', {
+            url: '/opportunities',
+            templateUrl: 'templates/opportunities.html'
+        });
+    })
+
+    .directive('myFirstDirective', function(){
+        return{
+            template: "<p>this is some content</p>",
+            restrict: "E"
+        }
+    });
